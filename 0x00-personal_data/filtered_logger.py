1d0368a518e0f22c9efa747e2b48801cff8f4021
imoleFolio <dremkay71@gmail.com>
2024-08-29 13:02:50 +0100

    task0: pycodestyle compliant

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -43,9 +43,8 @@ def filter_datum(fields: list[str], redaction: str,
                  message: str, separator: str) -> str:
     """returns the log message obfuscated:"""
     for field in fields:
-        message = \
-            re.sub(f"{field}=.*?{separator}",
-                   f"{field}={redaction}{separator}", message)
+        message = re.sub(f"{field}=.*?{separator}",
+                         f"{field}={redaction}{separator}", message)
     return message
 
 
