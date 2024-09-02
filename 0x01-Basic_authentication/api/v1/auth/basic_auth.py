8d9a47766ff536da2fe6f116428ce713fa6145e6
imoleFolio <dremkay71@gmail.com>
2024-09-02 16:18:51 +0100

    task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,6 +13,7 @@ Otherwise, keep the previous mechanism with auth an instance of Auth.
 In the first terminal:
 """
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -27,3 +28,17 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header.removeprefix('Basic ')
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ decode base64 str """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_str = decoded_bytes.decode('utf-8')
+            return decoded_str
+        except Exception:
+            return None
