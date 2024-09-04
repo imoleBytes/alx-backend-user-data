7682642d84ab70aa26b6e7aa5370b2490a02591a
imoleFolio <dremkay71@gmail.com>
2024-09-04 15:37:59 +0100

    task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -15,7 +15,20 @@ create an instance of SessionAuth and assign it to the variable auth
 Otherwise, keep the previous mechanism.
 """
 from .auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
-    ...
+    """ SessionAuth that inherits from Auth. """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ creates a Session ID for a user_id: """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        session_id = str(uuid4())
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+
+        return session_id
