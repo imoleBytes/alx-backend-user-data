484144e3c0b88ce141e2c9e9ae1f19b1338a9259
imoleFolio <dremkay71@gmail.com>
2024-09-04 17:19:56 +0100

    task 5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -33,11 +33,11 @@ def before_request():
         setattr(request, "current_user", auth.current_user(request))
         exclu_paths = [
             '/api/v1/status/', '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
+            '/api/v1/forbidden/', '/api/v1/auth_session/login/'
         ]
         if auth.require_auth(request.path, exclu_paths):
-            ...
-            if auth.authorization_header(request) is None:
+            cookie = auth.session_cookie(request)
+            if auth.authorization_header(request) is None and cookie is None:
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
