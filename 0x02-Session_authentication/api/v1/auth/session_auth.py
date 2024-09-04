9752d7b91a5574c04ba6cf6c952f1a55ba07a2f1
imoleFolio <dremkay71@gmail.com>
2024-09-04 17:36:37 +0100

    task 6

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -16,6 +16,7 @@ Otherwise, keep the previous mechanism.
 """
 from .auth import Auth
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -40,3 +41,12 @@ class SessionAuth(Auth):
         if not isinstance(session_id, str):
             return None
         return SessionAuth.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ returns a User instance based on a cookie value """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id:
+            user = User.get(user_id)
+            return user
+        return None
