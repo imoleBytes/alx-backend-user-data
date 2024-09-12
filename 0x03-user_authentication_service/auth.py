604883a1940412edcd2d6275aec64c9ab2383cfd
imoleFolio <dremkay71@gmail.com>
2024-09-12 13:04:44 +0100

    task 16

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -97,6 +97,16 @@ class Auth:
         except Exception:
             return None
 
+    def get_reset_password_token(self, email: str) -> str:
+        """ generate reset token and saves to the user and return it"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=token)
+        return token
+
 
 # if __name__ == "__main__":
 #     print(_hash_password("Hello Holberton"))
