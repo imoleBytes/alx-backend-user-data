67799a3790cf1241f930e583f9457633d501cfc2
imoleFolio <dremkay71@gmail.com>
2024-09-12 11:48:39 +0100

    task 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,7 +5,7 @@ In this task, you will set up a basic Flask app.
 Create a Flask app that has a single GET route ("/") and use flask.jsonify
 to return a JSON payload of the form:
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 
 from auth import Auth
 
@@ -33,6 +33,22 @@ def users() -> str:
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
     return jsonify({"email": f"{email}", "message": "user created"})
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+
+    response = make_response(
+        jsonify({"email": "<user email>", "message": "logged in"})
+    )
+    response.set_cookie("session_id", session_id)
+    return response
 
 
 if __name__ == "__main__":
